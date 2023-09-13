b5d8fd7be47bbbb25106c69c91e1d7eb1e0c703f
duycop <duycop@tnut.edu.vn>
2023-09-13 22:07:00 +0700

    thêm comment cho code c# trong API.aspx.cs

--- web_CamDo/api.aspx.cs
+++ web_CamDo/api.aspx.cs
@@ -12,28 +12,49 @@ namespace web_CamDo
     public partial class api : System.Web.UI.Page
     {
         const string cnStr = "Server=LAPTOP-DUYCOP\\SQLEXPRESS;Database=QL_Hieu_Cam_Do;User Id=sa;Password=123;";
+        /// <summary>
+        /// class chứa thông tin 1 khách hàng
+        /// </summary>
         class Data_ds_kh
         {
             public string tenkh, sdt, diachi, cccd;
         }
+
+        /// <summary>
+        /// hàm trả về ds kh ở dạng json string
+        /// đầu vào: không
+        /// đầu ra: all kh, string, format json
+        /// </summary>
         void ds_kh()
         {
             //truy van db: sp_khachHang @acion='ds_kh'
             //mong muốn trả về ...
             //mà trình duyệt show ra table html
             //họ và tên, sdt, dc, cccd
+
+            //kết nối tới csdl: tạo đối tượng, gán chuỗi kết nối (chứa thông tin để kết nối)
             SqlConnection cn = new SqlConnection(cnStr);
+            //tạo kết nối tới db
             cn.Open();
+
+            //sql chuẩn bị thực thi
             string sql = "SP_KhachHang";
+
+            //đối tượng sẽ thực thi sql trên kết nối đã mở
             SqlCommand cm = new SqlCommand(sql, cn);
+            //loại sql này là sp_
             cm.CommandType = CommandType.StoredProcedure;
+            //truyền các tham số để chuẩn bị chạy sp_
             cm.Parameters.Add("@action", SqlDbType.NVarChar, 50).Value = "ds_kh";
+            //thực thi sp_ , loại action này trả về dữ liệu, hứng vào dr
             SqlDataReader dr = cm.ExecuteReader();
+            //dt tạo trống để chuẩn bị load tất cả từ dr
             DataTable dt = new DataTable();
-            dt.Load(dr);
-            cm.Dispose();
-            cn.Close();
-            cn.Dispose();
+            dt.Load(dr); //load hết từ dr vào dt
+            cm.Dispose();  //hủy cm
+            cn.Close();    //đóng kết nối
+            cn.Dispose();  //giải phóng tài nguyên
+            ///lưu ý: luôn đóng, hủy khi dùng xong!
 
             //chuyển dt ->đối tượng->json string
 
@@ -49,16 +70,27 @@ namespace web_CamDo
                     k.cccd = r["cccd"].ToString();
                     L.Add(k);
                 }
-            //chuyển L->json
+            //chuyển obj L -> json text
             string json = JsonConvert.SerializeObject(L);
+
+            //phản hồi json text về trình duyệt
             this.Response.Write(json);
         }
 
+        /// <summary>
+        /// class này để chứa thông tin phản hồi về khi thêm 1 khách hàng
+        /// </summary>
         class Data_them_kh
         {
-            public bool ok;
-            public string error;
+            public bool ok; //true/false => báo thêm thành công hay ko
+            public string error; //nếu có lỗi thì chi tiết báo lỗi ở đây
         }
+        /// <summary>
+        /// hàm thêm 1 khách hàng vào db
+        /// input: 4 thông tin của 1 khách hàng
+        /// ouput: {ok: true, error: null} nếu thêm thành công
+        ///        {ok: false, error: "trùng sđt rồi nhé"} nếu thất bại 
+        /// </summary>
         void them_kh()
         {
             Data_them_kh R = new Data_them_kh();
@@ -87,33 +119,46 @@ namespace web_CamDo
                 cm.Parameters.Add("@sdt", SqlDbType.NVarChar, 50).Value = k.sdt;
                 cm.Parameters.Add("@diachi", SqlDbType.NVarChar, 500).Value = k.diachi;
                 cm.Parameters.Add("@cccd", SqlDbType.VarChar, 12).Value = k.cccd;
+                //loại action này ko trả về dữ liệu dạng bảng
+                //mà trả về số bản ghi bị tác động
                 int n = cm.ExecuteNonQuery();
 
                 if (n > 0)
                 {
+                    //thêm đc 1 bản ghi thành công thì n==1
                     R.ok = true;
                 }
                 else
                 {
+                    //n<=0 là sai rồi
                     R.ok = false;
                     R.error = "Lỗi gì đó nên ko thêm được";
                 }
-                cm.Dispose();
-                cn.Close();
-                cn.Dispose();
+                cm.Dispose(); //giải phóng tài nguyên thực thi sql
+                cn.Close();   //đóng kết nối
+                cn.Dispose(); //giải phóng tài nguyên kết nối db
             }
             catch (Exception ex)
             {
+                //bẫy được lỗi -> gán vào thuộc tính error
                 R.ok = false;
                 R.error = ex.Message;
             }
-            //chuyển R->json
+            //chuyển đối tượng R -> json text
             string json = JsonConvert.SerializeObject(R);
+            //gửi json text về trình duyệt
             this.Response.Write(json);
         }
+
+        /// <summary>
+        /// hàm này tự chạy khi api.aspx được gọi
+        /// </summary>
         protected void Page_Load(object sender, EventArgs e)
         {
+            //lấy biến action gửi lên
             string action = Request["action"];
+
+            //tùy theo action mà gọi hàm tương ứng để xử lý
             switch (action)
             {
                 case "ds_kh":
