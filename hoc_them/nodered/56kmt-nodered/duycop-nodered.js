7f457a37ddfa67be32f48c5b5a313ab62f89f4d2
duycop <duycop@LAPTOP-DUYCOP>
2023-09-26 23:26:00 +0700

    làm đẹp code

--- hoc_them/nodered/56kmt-nodered/duycop-nodered.js
+++ hoc_them/nodered/56kmt-nodered/duycop-nodered.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  $("#nut-xem").click(function(){
-    $.post("api.php",
-    {
-      action: "xem_diem",
-      masv: $("#masv").val()
-    },
-    function(data,status){
-      //console.log([data,status]) //test done đúng ý
-      $("#kq").html(data)
-    });
-  });
+	$("#nut-xem").click(function(){
+		$.post("api.php",
+		{
+			action: "xem_diem",
+			masv: $("#masv").val()
+		},
+		function(data,status){
+			//console.log([data,status]) //test done đúng ý
+			$("#kq").html(data)
+		});
+	});
 });
