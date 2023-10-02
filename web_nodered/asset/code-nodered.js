b76fc21ec5c0d8f85db3aa94478f80bae44f3220
duycop <duycop@LAPTOP-DUYCOP>
2023-10-02 21:36:04 +0700

    web TTS

--- web_nodered/asset/code-nodered.js
+++ web_nodered/asset/code-nodered.js
@@ -37,14 +37,46 @@ $(document).ready(function () {
     $.get(url, { /* ko gửi gì! */ },
       function (json) {
         //khi tải xong, nhận đc phản hồi là json
-        var s = json.Value + '  at time: ' + json.Time.replace('T', ' ').replace('.000Z', '');
+        var s = json.value + '  at time: ' + json.time.replace('T', ' ').replace('Z', '');
+        s += " min=" + json.min + " max=" + json.max;
+        if (json.value > json.max) {
+          var sos_text = "SOS " + json.name + " là " + json.value + " " + json.unit + ", cao quá ngưỡng " + json.max + " " + json.unit +" rồi";
+          play_tts(sos_text)
+          s += '<br><DIV class="sos">' + sos_text +'</DIV>'
+        }
         $('#kq-ss').html(s);
-        if (json.loai == 'nhietdo')drawChart_nhietdo(json.Value) //loại này vì vẽ đồng hồ
+        if (json.loai == 'nhietdo')drawChart_nhietdo(json.value) //loại này vì vẽ đồng hồ
         setTimeout(function () { get_sensor() },1000);
+      });//end ajax post
+  }
+  var gtts_playing = 0;             //biến đánh dấu xem có đang bận play ko
+  function playSound(url) {         //hàm này nhận 1 url audio để play
+    if (gtts_playing) return;       //nếu đang bận playing thì thoát
+    var audio = new Audio(url);     //tạo đối tượng để chuẩn bị play
+    audio.play().then(() => {       //gọi play() và khi thực sự play thì:
+      gtts_playing = 1;             //đánh dấu đang play
+      console.log('mp3 play');      //debug ra console
+    });                             //hết hàm play
+    audio.addEventListener("ended", //đăng ký sự kiện play xong thì:
+      function () {                 //play xong thì hàm này chạy
+        gtts_playing = 0;           //bỏ đánh dấu bận => hết bận
+        console.log('mp3 stop');    //debug ra cosole là stop rồi
+    });                             //hết hàm xử lý stop
+  }                                 //hết hàm playSound
+
+  function play_tts(txt){               //nhận txt là chuỗi cần tts
+    $.post("http://127.0.0.1:1880/mp3", //gọi API của nodered
+      {txt:txt},                        //truyền lên chuỗi cần tts
+      function (audio_fn) {             //nhận về tên file mp3
+        playSound('/mp3/' + audio_fn);  //play url=file này trong thư mục mp3
+        $('#kq-audio-mp3').html('đang play: ' + audio_fn) //show lên web (để debug là chính)
       });//end ajax post
   }
   $('.btn-sode').click(function () { get_so_de(); });
   $('.btn-sensor').click(function () { get_sensor(); });
   $('#choice-sensor').on("change", function () { get_sensor(); });
+
+  $('.btn-audio-mp3').click(function () { play_tts($('#what_talk').val()); });
+
   list_sensor(); //tự gọi
 }); //end ready
