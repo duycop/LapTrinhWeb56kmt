211c45cae9d98eb916f82a95ce6c83caedab42d2
duycop <duycop@LAPTOP-DUYCOP>
2023-10-04 19:59:58 +0700

    Cookie

---
+++ QL_DRL/api.aspx.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QL_DRL
+{
+    public partial class api : System.Web.UI.Page
+    {
+        private const string cnStr = "Server=127.0.0.1;Database=QL_DRL;User Id=sa;Password=123;";
+        
+        class Reply
+        {
+            public bool ok;
+            public string msg;
+        }
+        class Reply_Check_cc: Reply
+        {
+            public int masv;
+            public string hoten, lop, cc;
+        }
+        void login()
+        {
+            Reply_Check_cc reply = new Reply_Check_cc();
+            try
+            {
+                int masv = int.Parse(this.Request["masv"]);
+                string pw = this.Request["pw"];
+
+                //truy van db
+                string sql = "SP_LOGIN";
+                SqlConnection cn = new SqlConnection(cnStr);
+                cn.Open();
+                SqlCommand cm = new SqlCommand(sql, cn);
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Parameters.Add("@action", SqlDbType.NVarChar, 50).Value = "login";
+                cm.Parameters.Add("@masv", SqlDbType.Int).Value = masv;
+                cm.Parameters.Add("@pw", SqlDbType.NVarChar, 50).Value = pw;
+                SqlDataReader dr = cm.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dr.Close();
+                cm.Dispose();
+                if (dt.Rows.Count > 0)
+                {
+                    //login ok: tra ve json {ok:true,masv:104,hoten:"nguyen van A",lop:"56kmt"}
+                    reply.masv = masv;
+                    reply.hoten = dt.Rows[0]["hoten"].ToString();
+                    reply.lop = dt.Rows[0]["lop"].ToString();
+                    reply.cc = dt.Rows[0]["cc"].ToString();
+                    reply.msg = "đúng rồi";
+                    reply.ok = true;
+                }
+                else
+                {
+                    //bao loi
+                    reply.ok = false;
+                    reply.msg = "có gì đó sai sai rồi";
+                }
+            }
+            catch (Exception ex)
+            {
+                reply.ok = false;
+                reply.msg = ex.Message;
+            }
+            string json = JsonConvert.SerializeObject(reply);
+            this.Response.Write(json);
+        }
+        void check_cc()
+        {
+            Reply_Check_cc reply = new Reply_Check_cc();
+            try
+            {
+                string cc = this.Request["cc"];
+                int masv = int.Parse(this.Request["masv"]);
+
+                //truy van db
+                string sql = "SP_LOGIN";
+                SqlConnection cn = new SqlConnection(cnStr);
+                cn.Open();
+                SqlCommand cm = new SqlCommand(sql, cn);
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Parameters.Add("@action", SqlDbType.NVarChar, 50).Value = "check_cc";
+                cm.Parameters.Add("@masv", SqlDbType.Int).Value = masv;
+                cm.Parameters.Add("@cc", SqlDbType.VarChar, 50).Value = cc;
+                SqlDataReader dr = cm.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dr.Close();
+                cm.Dispose();
+                if (dt.Rows.Count > 0)
+                {
+                    //login ok: tra ve json {ok:true,masv:104,hoten:"nguyen van A",lop:"56kmt"}
+                    reply.masv = masv;
+                    reply.hoten = dt.Rows[0]["hoten"].ToString();
+                    reply.lop = dt.Rows[0]["lop"].ToString();
+                    reply.msg = "đúng rồi";
+                    reply.ok = true;
+                }
+                else
+                {
+                    //bao loi
+                    reply.ok = false;
+                    reply.msg = "có gì đó sai sai rồi";
+                }
+            }catch(Exception ex) {
+                reply.ok = false;
+                reply.msg=ex.Message;
+            }
+            string json = JsonConvert.SerializeObject(reply);
+            this.Response.Write(json);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string action = this.Request["action"];
+            switch (action)
+            {
+                case "login":
+                    login();
+                    break;
+                case "check_cc":
+                    check_cc();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ QL_DRL/asset/code-login.js
@@ -0,0 +1,123 @@
+﻿$(document).ready(function () {
+  function form_login() {
+    console.log('login form show nào...')
+    //show nút login
+    $('#cmd-login').show();
+    //ẩn welcome
+    $('#welcome-user').hide();
+    noi_dung_login = `
+<span class="login100-form-title">
+Member Login
+</span>
+<div>
+<input class="dai-vua-vua" type="text" id="masv" placeholder="Mã SV">
+</div>
+<div>
+<input class="dai-vua-vua" type="password" id="pw" placeholder="Password">
+</div>
+<div id='dang-nhap-sai'></div>
+    `;
+    var dialog_login = $.confirm({
+      title: 'Đăng nhập',
+      content: noi_dung_login,
+      buttons: {
+        OK: {
+          text: "Đăng nhập",
+          keys: ['enter'],
+          btnClass: 'btn-blue',
+          action: function () {
+            check_login();
+            return false; //để hộp thoại nó ko đóng
+          }
+        },
+        Cancel: {
+          text: "Hủy",
+          btnClass: 'btn-red',
+        }
+      }
+    });
+    function check_login() {
+      //lấy dc masv+pw người dùng nhập
+      var masv = $('#masv').val();
+      var pw = $('#pw').val();
+
+      $.post('api.aspx',
+        {
+          action: 'login',
+          masv: masv,
+          pw: pw
+        },
+        function (data) {
+          var json = JSON.parse(data);
+          if (json.ok) {
+            //chào
+            chao_hoi(json);
+            //lưu cookie
+            setCookie("masv",json.masv, 0)
+            setCookie("cc", json.cc, 0)
+            dialog_login.close(); //đóng form lại vì nó hoàn thành nhiệm vụ rồi
+          } else {
+            //show form login
+            //form_login(); //form ko đóng, nên ko cần show mới
+            $('#dang-nhap-sai').html(json.msg)
+          }
+        }
+      )
+    }
+  }
+
+  function chao_hoi(json) {
+    if (json.ok) {
+      $('#welcome-user').html("Xin chào " + json.hoten);
+      $('#welcome-user').show();
+      $('#cmd-login').hide();
+      $('#cmd-logout').show();
+    } else {
+      $('#welcome-user').html('');
+      $('#welcome-user').hide();
+      $('#cmd-login').show();
+      $('#cmd-logout').hide();
+    }
+  }
+  function check_cc() {
+    //nếu ko có cc thì ko cần gửi
+    //gửi cc(nếu có) lên api
+    //nhận về OK: chào hoten
+    //nếu not ok: show form login
+    //var cc = getCookie('cc');
+    //var masv = getCookie('masv');
+    //if (cc && masv)
+    if(1==1)
+    {
+      console.log('check cc trên api xem cc này ok ko?')
+      $.post('api.aspx',
+        {
+          action: 'check_cc',
+          //masv: masv,  //cookie thì auto gửi, ko cần coder gửi
+          //cc: cc
+        },
+        function (data) {
+          var json = JSON.parse(data);
+          if (json.ok) {
+            chao_hoi(json);
+          } else {
+            //show form login
+            form_login();
+          }
+        }
+      )
+    } else {
+      console.log('ko thấy cc trên máy nên ko gửi lên api')
+      form_login();
+    }
+  }
+  function do_logout() {
+    eraseCookie("masv");
+    eraseCookie("cc");
+    chao_hoi({ok:false})
+    form_login();
+  }
+  $('#cmd-login').click(function(){ form_login(); });
+  $('#cmd-logout').click(function(){ do_logout(); });
+  check_cc(); //tự gọi hàm này mỗi khi tải trang xong
+}); //end ready
