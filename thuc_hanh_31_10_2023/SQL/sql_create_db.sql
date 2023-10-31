4e5183e5a52c7985a231837f921cbdf33e42bbf2
duycop <duycop@LAPTOP-DUYCOP>
2023-10-31 10:04:56 +0700

    thêm sql

---
+++ thuc_hanh_31_10_2023/SQL/sql_create_db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE [ThucHanh]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'SuatAn', FILENAME = N'd:\ThucHanh.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'SuatAn_log', FILENAME = N'd:\ThucHanh_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+CREATE TABLE [dbo].[Company](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[address] [nvarchar](255) NOT NULL,
+	[lat] [float] NULL,
+	[lng] [float] NULL,
+	[phone] [varchar](50) NULL,
+	[del_at] [datetime] NULL,
+ CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
